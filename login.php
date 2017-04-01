d91b55630b6fdfc0dc58b9148fbd745807148061
abihas <info@michaelnieuwenweg.nl>
2017-04-01 12:25:42 +0200

    login pagina

--- login.php
+++ login.php
@@ -10,11 +10,35 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css"
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
+    
+    <!-- login formulier -->
+    <form class="inloggen">
+        <h2>Inloggen</h2>
+        
+        <label for="inputEmail" class="sr-only">E-mailadres</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="E-mailadres" required="" autofocus="">
+        
+        <label for="inputPassword" class="sr-only">Wachtwoord</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Wachtwoord" required="">
+        
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+
+        <button class="btn btn-lg btn-danger btn-block" type="submit">Log in</button>
+      </form>
+      
+
+
+
     <script src="js/jquery-3.2.0.min.js"></script>    
     <script src="js/bootstrap.min.js"></script>
     </body>
