5565c8de51556252ff96d25d7a4dd310132b29a1
dmini1 <m.nieuwenweg@wmd.nl>
2017-04-03 12:53:58 +0200

    Hoofdstructuur

---
+++ parts/header.php
@@ -0,0 +1,6 @@
+<div class="header">
+      <div class="container text-center">
+        <h1 class="bedrijfsnaam">PHP Pizza</h1>
+        <p class="lead beschrijving">The best in town</p>
+      </div>
+</div>
---
+++ parts/navigation.php
@@ -0,0 +1,42 @@
+<div class="col-sm-3 offset-sm-1">
+    <div class="navigatie">
+
+        <button type="button" class="btn btn-default btn-lg btn-block">
+            <span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home
+        </button>
+
+        <button type="button" class="btn btn-default btn-lg btn-block">
+            Menukaart
+        </button>
+
+        <button type="button" class="btn btn-default btn-lg btn-block">
+            Bestellen
+        </button>
+
+        <button type="button" class="btn btn-default btn-lg btn-block invisible">
+            Bestellen
+        </button>
+
+        <div class="btn-group btn-block">
+            <button type="button" class="btn btn-default btn-lg dropdown-toggle btn-block" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Wie wij zijn <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Foto's</a></li>
+            </ul>
+        </div>
+
+        <div class="btn-group btn-block">
+            <button type="button" class="btn btn-default btn-lg dropdown-toggle btn-block" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Contact <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+                <li><a href="#">Contactgegevens</a></li>
+                <li><a href="#">Openingstijden</a></li>
+                <li><a href="#">Veelgestelde vragen</a></li>
+            </ul>
+        </div>
+
+    </div>
+</div>
