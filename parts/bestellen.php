4302e863e596deb052a2a6b637fba36ae9815e58
dmini1 <m.nieuwenweg@wmd.nl>
2017-04-04 10:46:38 +0200

    producten

--- parts/bestellen.php
+++ parts/bestellen.php
@@ -1,26 +1,85 @@
-<div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-block">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+<div class="row">
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://vvhegelsom.nl/wp-content/uploads/2016/11/Pizza-Free-PNG-Image.png" alt="...">
+      <div class="caption">
+        <h3>Margherita</h3>
+        <p>De pizza margherita is een typisch Napolitaanse pizza met als ingrediënten tomaten, mozzarella, verse basilicum, zout en olijfolie. Samen met de andere twee Napolitaanse pizza's, de pizza marinara en de pizza margherita extra valt deze onder een keurmerk van de Europese Unie.</p>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button class="btn btn-danger" type="button"> - </button>
+                <button class="btn btn-primary" type="button"> + </button>
+              </span>
+              <input type="text" class="form-control" placeholder=" 0 ">
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <button class="btn btn-primary pull-right" type="button">Bestel</button>
+          </div>
+        </div><!-- /.row -->
+
+      </div>
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-block">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+   <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://vvhegelsom.nl/wp-content/uploads/2016/11/Pizza-Free-PNG-Image.png" alt="...">
+      <div class="caption">
+        <h3>Margherita</h3>
+        <p>De pizza margherita is een typisch Napolitaanse pizza met als ingrediënten tomaten, mozzarella, verse basilicum, zout en olijfolie. Samen met de andere twee Napolitaanse pizza's, de pizza marinara en de pizza margherita extra valt deze onder een keurmerk van de Europese Unie.</p>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button class="btn btn-danger" type="button"> - </button>
+                <button class="btn btn-primary" type="button"> + </button>
+              </span>
+              <input type="text" class="form-control" placeholder=" 0 ">
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <button class="btn btn-primary pull-right" type="button">Bestel</button>
+          </div>
+        </div><!-- /.row -->
+
+      </div>
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-block">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+   <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://vvhegelsom.nl/wp-content/uploads/2016/11/Pizza-Free-PNG-Image.png" alt="...">
+      <div class="caption">
+        <h3>Margherita</h3>
+        <p>De pizza margherita is een typisch Napolitaanse pizza met als ingrediënten tomaten, mozzarella, verse basilicum, zout en olijfolie. Samen met de andere twee Napolitaanse pizza's, de pizza marinara en de pizza margherita extra valt deze onder een keurmerk van de Europese Unie.</p>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <button class="btn btn-danger" type="button"> - </button>
+                <button class="btn btn-primary" type="button"> + </button>
+              </span>
+              <input type="text" class="form-control" placeholder=" 0 ">
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <button class="btn btn-primary pull-right" type="button">Bestel</button>
+          </div>
+        </div><!-- /.row -->
+
+      </div>
     </div>
   </div>
+
+
 </div>
