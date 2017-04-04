c86d3e70dc1eb39402ce23e8cda7fdcb51ce9f47
rtenkloooster <github@tenklooster.net>
2017-04-05 00:03:41 +0200

    Get Chart Detail added

--- functions/get_chart.php
+++ functions/get_chart.php
@@ -18,11 +18,33 @@ function get_cart_total(){
 }
 }
 
+function get_cart_total(){
+  // Controleer of de cart gevuld is
+  if(isset($_SESSION['cart'])){
+  if(is_array($_SESSION['cart'])){
+    foreach($_SESSION['cart'] AS $id => $amount){
+        // Haal hier de data op:
+        // 1. Pizza Bolognese Small   1x    16,95   16,95   + - X
+        // 2. Pizza Bolognese Small   1x    16,95   16,95   + - X
+        // 3. Pizza Bolognese Small   1x    16,95   16,95   + - X
+        // 4. Totaal:                 3x            41,95
+        echo "Mandindhoud";
+    }
+
+  }else{
+    echo "De winlelmand bevat nog geen items.";
+  }
+}else{
+  echo "De winkelmand bevat nog geen items.";
+}
+}
 // Er kan gevraagd worden om totaal bedrag van het mandje (action = getTotal)
 // Er kan gevraagd worden om alle order regels (getCart)
 if($action == "getTotal"){
   get_cart_total();
 }
-
+if($action == "getCart"){
+  get_cart_detail();
+}
 
 ?>
