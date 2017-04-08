20ee0c93b2e68398c9e7576edec3341efb6d4ee0
abihas <info@michaelnieuwenweg.nl>
2017-04-09 00:05:11 +0200

    openingstijden

--- parts/openingstijden.php
+++ parts/openingstijden.php
@@ -1,1 +1,31 @@
-openingstijden
+<table class="table">
+    <tr>
+        <td>Maandag</td>
+        <td>16:00 - 23:00</td>
+    </tr>
+    <tr>
+        <td>Dinsdag</td>
+        <td>16:00 - 23:00</td>
+    </tr>
+    <tr>
+        <td>Woensdag</td>
+        <td>16:00 - 23:00</td>
+    </tr>
+    <tr>
+        <td>Donderdag</td>
+        <td>16:00 - 23:00</td>
+    </tr>
+    <tr>
+        <td>Vrijdag</td>
+        <td>16:00 - 23:00</td>
+    </tr>
+    <tr>
+        <td>Zaterdag</td>
+        <td>16:00 - 23:00</td>
+    </tr>
+    <tr>
+        <td>Zondag</td>
+        <td>16:00 - 23:00</td>
+    </tr>
+</table>
+<p class="lead">We zijn ook geopend tijdens zon- en feestdagen</p>
