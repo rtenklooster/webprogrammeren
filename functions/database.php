70bdcaaffe7d25ad5b81284bf17fbc1ec822f0d6
rtenkloooster <github@tenklooster.net>
2017-04-08 22:49:24 +0200

    Identificeer server
    Verschillende SQL gegevens

--- functions/database.php
+++ functions/database.php
@@ -1,9 +1,16 @@
 <?php
+$hostname = getHostName();
+if($hostname == "MacBook-Pro-van-Richard.local"){
 $host = "localhost";
-$user = "abihas";
-$pass = "bdgoiusdfgjhs987r2342098fgdgbdk";
+$user = "root";
+$pass = "";
 $name = "pizza";
-
+}else{
+  $host = "localhost";
+  $user = "abihas";
+  $pass = "bdgoiusdfgjhs987r2342098fgdgbdk";
+  $name = "pizza";
+}
 try
 {
      $db = new PDO("mysql:host={$host};dbname={$name}",$user,$pass);
