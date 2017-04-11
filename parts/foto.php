25cf3f43c432bdc04094154b97529fce36763cff
abihas <info@michaelnieuwenweg.nl>
2017-04-11 22:14:46 +0200

    interieur foto's

--- parts/foto.php
+++ parts/foto.php
@@ -1,26 +1,59 @@
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel" data-slide-to="1" class="active"></li>
+    <li data-target="#carousel" data-slide-to="2"></li>
+    <li data-target="#carousel" data-slide-to="3"></li>
+    <li data-target="#carousel" data-slide-to="4"></li>
+    <li data-target="#carousel" data-slide-to="5"></li>
+    <li data-target="#carousel" data-slide-to="6"></li>
+    <li data-target="#carousel" data-slide-to="7"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="http://vvhegelsom.nl/wp-content/uploads/2016/11/Pizza-Free-PNG-Image.png" alt="...">
+      <img src="img/interieur/1.jpg" alt="...">
       <div class="carousel-caption">
-        Pizza
+        Ons gezellige interieur
       </div>
     </div>
     <div class="item">
-      <img src="http://vvhegelsom.nl/wp-content/uploads/2016/11/Pizza-Free-PNG-Image.png" alt="...">
+      <img src="img/interieur/2.jpg" alt="...">
       <div class="carousel-caption">
-        Nog meer pizza
+        Ons gezellige interieur
       </div>
     </div>
-    ...
+    <div class="item">
+      <img src="img/interieur/3.jpg" alt="...">
+      <div class="carousel-caption">
+        Ons gezellige interieur
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/interieur/4.jpg" alt="...">
+      <div class="carousel-caption">
+        Ons gezellige interieur
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/interieur/5.jpg" alt="...">
+      <div class="carousel-caption">
+        Ons gezellige interieur
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/interieur/6.jpg" alt="...">
+      <div class="carousel-caption">
+        Ons gezellige interieur
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/interieur/7.jpg" alt="...">
+      <div class="carousel-caption">
+        Ons gezellige interieur
+      </div>
+    </div>
   </div>
 
   <!-- Controls -->
