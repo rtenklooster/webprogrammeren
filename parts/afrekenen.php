1b6a5175cc08718d68b13f013160397a0fc8eb0b
abihas <info@michaelnieuwenweg.nl>
2017-04-12 11:21:52 +0200

    adresgegevens

--- parts/afrekenen.php
+++ parts/afrekenen.php
@@ -45,6 +45,50 @@ if($sampleArray) {
     </table>
     </div>
 
+    <h3>Afleveradres</h3>
+    <form>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-xs-12">
+                    <label for="naam">Naam</label>
+                    <input type="text" name="naam" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-8">
+                    <label for="straat">Straat</label>
+                    <input type="text" name="straat" class="form-control">
+                </div>
+                <div class="col-xs-4">
+                    <label for"huisnummer">Huisnummer</label>
+                    <input type="text" name="huisnummer" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4">
+                    <label for="postcode">Postcode</label>
+                    <input type="text" name="postcode" class="form-control" pattern="^[1-9][0-9]{3} ?(?!sa|sd|ss|SA|SD|SS)[A-Za-z]{2}$">
+                </div>
+                <div class="col-xs-8">
+                    <label for="woonplaats">Woonplaats</label>
+                    <input type="text" name="woonplaats" class="form-control">
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <h3>Betaalmethode</h3>
+    <div class="dropdown">
+         <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+             Kies <span class="caret">
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <li id="ideal"><a href="#">iDeal</a></li>
+            <li id="bezorger"><a href="#">Aan de bezorger</a></li>
+        </ul>
+
+    </div>
+
     <button class="btn btn-primary btn-lg pull-right">Bestel</button>
 
     <?php
