dde68aba2408723a1b885d3a31aad0e12f5c2e86
sejkuiper <sejkuiper@hotmail.com>
2017-04-13 18:26:37 +0200

    taalfoutje

--- parts/overons.php
+++ parts/overons.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-8">
         <div class="">
-			"Sinds 1997 zijn wij gevestigd in Groningen. En inmiddels zijn wij een bekend begrip in Groningen. Wij zijn 5 enthousiaste PHP-bakkers die gericht zijn op het tevreden stellen van onze klanten met smaakvolle en visueel aantrekkelijke producten. Hieronder ziet u een foto van ons team. Als u ons bezoekt, zult u ons onmiddellijk herkennen aan onze blije hoofden. Wij staan u graag te woord als u vragen hebt over de mogelijkheden van ons PHP-restaurant"    "
+			"Sinds 1997 zijn wij gevestigd in Groningen. En inmiddels zijn wij een bekend begrip in Groningen. Wij zijn 5 enthousiaste PHP-bakkers die gericht zijn op het tevreden stellen van onze klanten met smaakvolle en visueel aantrekkelijke producten. Hieronder ziet u een foto van ons team. Als u ons bezoekt, zult u ons onmiddellijk herkennen aan onze blije hoofden. Wij staan u graag te woord als u vragen hebt over de mogelijkheden van ons PHP-restaurant"
         </div>
     </div>
     <div class="col-md-4">
