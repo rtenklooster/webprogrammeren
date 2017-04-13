1c0199383f3b8c891adce626b1e884b570a46393
sejkuiper <sejkuiper@hotmail.com>
2017-04-13 16:11:44 +0200

    Update veelgesteldevragen.php

--- parts/veelgesteldevragen.php
+++ parts/veelgesteldevragen.php
@@ -4,16 +4,13 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-            Vraag 1
+             Is het verplicht om te reserveren?
         </a>
       </h4>
     </div>
     <div id="collapse1" class="panel-collapse collapse in">
       <div class="panel-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        Reserveren is niet verplicht. Wij adviseren, om teleurstelling te voorkomen, dit wel te doen. Wij kunnen u op deze manier een plaats garanderen. Neemt u contact met ons op om de reservering door te geven.
       </div>
     </div>
   </div>
@@ -22,32 +19,58 @@
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
-            Vraag 2
+            Hebben jullie cadeaubonnen of accepteren jullie dinercheques?
         </a>
       </h4>
     </div>
     <div id="collapse2" class="panel-collapse collapse">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
+     <div class="panel-body">
+        Helaas accepteren wij geen dinercheques/dinerbonnen/VVV bonnen.
+     </div>
     </div>
   </div>
-
+  
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+            Wij willen met een grote groep komen. Is een groepskorting mogelijk?
+        </a>
+      </h4>
+    </div>
+    <div id="collapse2" class="panel-collapse collapse">
+     <div class="panel-body">
+        Wij kunnen heleaas geen korting geven op de prijs. Als u ons tijdig op de hoogte stelt van uw groepsgrootte doen wij uiteraard ons best om u een goede plek te geven waarbij u bij elkaar kunt zitten.
+     </div>
+    </div>
+  </div>
+  
+    <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+            Is er een kinderhoek?
+        </a>
+      </h4>
+    </div>
+    <div id="collapse2" class="panel-collapse collapse">
+     <div class="panel-body">
+        We beschikken niet over een kinderhoek in ons restaurant; wel zijn er kleurplaten en kleurtjes aanwezig voor onze kleine gasten.
+     </div>
+    </div>
+  </div>
+  
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-            Vraag 3
+            Zijn huisdieren toegestaan in het restaurant?
         </a>
       </h4>
     </div>
     <div id="collapse3" class="panel-collapse collapse">
       <div class="panel-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+       Wij vragen van u om geen hond of andere dieren mee te nemen. De ervaring heeft ons geleerd dat andere gasten niet altijd gecharmeerd zijn van dieren tijdens hun restaurantbezoek.
       </div>
     </div>
   </div>
