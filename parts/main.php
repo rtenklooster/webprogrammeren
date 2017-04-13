364a2757b7bf7c8d153978842a101e402f0e1189
abihas <info@michaelnieuwenweg.nl>
2017-04-13 15:20:00 +0200

    Homepage

--- parts/main.php
+++ parts/main.php
@@ -1,3 +1,11 @@
-    <div class="">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+<div class="row">
+    <div class="col-md-8">
+        <div class="">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        
+        </div>
     </div>
+    <div class="col-md-4">
+        <img src="img/home.png" class="img-responsive img-rounded pull-right" alt="Responsive image">
+    </div>
+</row>
