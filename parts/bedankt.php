8015405b2d2e45fd4d1186578b9642c032bd82f8
abihas <info@michaelnieuwenweg.nl>
2017-04-13 21:02:11 +0200

    bestelling naar database :-)

--- parts/bedankt.php
+++ parts/bedankt.php
@@ -1,5 +1,5 @@
 <?php
-var_dump($_POST);
+// var_dump($_POST);
 require_once('functions/database.php');
 
 submitOrder();
@@ -17,35 +17,35 @@ function submitOrder() {
             break;
         }
 
-        // $bestel_datumtijd = date("Y-m-d H:i:s");
-        // $gewenste_moment = date("Y-m-d H:i:s");
+        $bestel_datumtijd = date("Y-m-d H:i:s");
+        $gewenste_moment = date("Y-m-d H:i:s");
+        // var_dump($bestel_datumtijd);
     
         global $db;
-        $sql = 'INSERT INTO klant (emailadres, voornaam,tussenvoegsel,achternaam,adres,postcode,woonplaats,telefoonnummer)
-                VALUES (:emailadres, 
-                        :voornaam,
-                        :tussenvoegsel,
-                        :achternaam,
-                        :adres,
-                        :postcode 
-                        :woonplaats, 
-                        :telefoonnummer
-                        );';
-
+        $sql = 'INSERT INTO klant (emailadres, voornaam, tussenvoegsel, achternaam, adres, postcode, woonplaats, telefoonnummer)
+                VALUES (:emailadres, :voornaam, :tussenvoegsel, :achternaam, :adres, :postcode, :woonplaats, :telefoonnummer);';
         $stmt = $db->prepare($sql);
-
+        $stmt->bindParam(':emailadres', $email, PDO::PARAM_STR);
         $stmt->bindParam(':voornaam', $voornaam, PDO::PARAM_STR);
         $stmt->bindParam(':tussenvoegsel', $tussenvoegsel, PDO::PARAM_STR);
         $stmt->bindParam(':achternaam', $achternaam, PDO::PARAM_STR);
         $stmt->bindParam(':adres', $adres, PDO::PARAM_STR);
         $stmt->bindParam(':postcode', $postcode, PDO::PARAM_STR);
         $stmt->bindParam(':woonplaats', $woonplaats, PDO::PARAM_STR);
-        $stmt->bindParam(':emailadres', $email, PDO::PARAM_STR);
         $stmt->bindParam(':telefoonnummer', $telefoonnummer, PDO::PARAM_STR);
-        
-        // $stmt->bindParam(':bestel_datumtijd', $bestel_datumtijd, PDO::PARAM_INT);
-        // $stmt->bindParam(':betaalmethode', $betaalmethodeint, PDO::PARAM_INT);
-        // $stmt->bindParam(':gewenste_moment', $gewenste_moment, PDO::PARAM_INT);
+        $stmt->execute();
+
+        // klant id is nodig om de bestelling toe te kunnen voegen
+        $id = $db->lastInsertId();
+
+        $sql = 'INSERT INTO bestelling (klant_id, bestel_datumtijd, betaalmethode, gewenste_moment)
+        VALUES (:klant_id, :bestel_datumtijd, :betaalmethode, :gewenste_moment);';
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':klant_id', $id);
+        $stmt->bindParam(':bestel_datumtijd', $bestel_datumtijd);
+        $stmt->bindParam(':betaalmethode', $betaalmethodeint);
+        $stmt->bindParam(':gewenste_moment', $gewenste_moment);
 
         $stmt->execute();
     }
