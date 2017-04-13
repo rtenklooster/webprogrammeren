09b1e23cc6662657f71a7cc657a7e3fa17c82559
sejkuiper <sejkuiper@hotmail.com>
2017-04-13 18:09:46 +0200

    tekst hoofdpagina

--- parts/main.php
+++ parts/main.php
@@ -1,8 +1,7 @@
 <div class="row">
     <div class="col-md-8">
         <div class="">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        
+			"Welkom op onze site van PHP Pizza! Ons sfeervolle restaurant is gevestigd in de historische binnenstad van het gezellige Groningen. Wij zijn hier sinds een aantal jaren een bekendheid onder liefhebbers van de Italiaanse keuken. Bij ons kunt u terecht voor pizza's, pasta's en diverse drankjes. De foto's geven een goede impressie van de sfeer in ons restaurant. Kijkt u vooral eens rond op de site. U kunt niet alleen eten in ons restaurant; we hebben ook een bestel-service. Via de knop 'bestellen' hoeft u uw huis niet uit komen. Wij bezorgen dan aan huis. En neemt u vooral contact op als u vragen hebt! We staan u graag te woord om u uiteindelijk te verwelkomen in ons restaurant. "
         </div>
     </div>
     <div class="col-md-4">
