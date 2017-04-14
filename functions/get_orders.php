b7eef192636f1bf887b80aff678162644ad9096e
rtenklooster <github@tenklooster.net>
2017-04-14 15:16:26 +0200

    get orders

--- functions/get_orders.php
+++ functions/get_orders.php
@@ -1,6 +1,25 @@
 <?php
 
 function getOrders() {
-    
+  global $db;
+  $sql = '
+    SELECT DISTINCT bestelling.id, bestelling.bestel_datumtijd, bestelling.gewenste_moment, klant.voornaam, klant.tussenvoegsel, klant.achternaam, klant.adres, klant.postcode, klant.woonplaats, klant.telefoonnummer
+    FROM bestelling
+    JOIN klant ON bestelling.klant_id = klant_id;';
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return($result);
 }
 ?>
+
+<pre>
+  <?php
+    foreach(getOrders() AS $order){
+      echo $order['id'];
+      echo "<br>";
+    }
+  print_r(getOrders()); ?>
+</pre>
--- parts/bestellingen.php
+++ parts/bestellingen.php
@@ -4,6 +4,7 @@
 if(!$_SESSION['logged_in']){
     echo "Helaas u heeft geen rechten om deze pagina te bezoeken.";
   }else{
+    require_once('functions/get_orders.php');
     // Haal de bestellingen uit de database
     $bestellingArray = getOrders();
 ?>
