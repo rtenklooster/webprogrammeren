fc57af180e01e993adbc389b5a604eceab7c0252
pawesseling <p.a.wesseling@st.hanze.nl>
2017-04-14 15:23:26 +0200

    sql aangepast

--- sql/pizza_database.sql
+++ sql/pizza_database.sql
@@ -8,7 +8,6 @@ CREATE TABLE productcategorie (
 PRIMARY KEY(id)
 );
 
-
 CREATE TABLE klant (
   id                         	INTEGER UNSIGNED         	NOT NULL     AUTO_INCREMENT,
   emailadres                 	varchar(60)              	NOT NULL,
@@ -22,12 +21,21 @@ CREATE TABLE klant (
   PRIMARY KEY(id)
 );
 
+CREATE TABLE autorisatie (
+  id                         	INTEGER UNSIGNED         	NOT NULL     AUTO_INCREMENT,
+  naam                       	varchar(40)              	NOT NULL,
+PRIMARY KEY(id)
+);
+
+
 CREATE TABLE user (
   id                         	INTEGER UNSIGNED         	NOT NULL     AUTO_INCREMENT,
   emailadres                 	varchar(60)              	NOT NULL,
   password                  	varchar(128)             	NOT NULL,
   naam                   	    varchar(40),
+  autorisatie_id				INTEGER UNSIGNED         	NOT NULL,
   PRIMARY KEY(id),
+  FOREIGN KEY(autorisatie_id)    		REFERENCES autorisatie(id),
   UNIQUE (emailadres)
 );
 
@@ -38,6 +46,7 @@ CREATE TABLE product (
   omschrijving               	text,
   categorie_id               	INTEGER UNSIGNED         	NOT NULL,
   prijs              			int(10)                   	NOT NULL,
+  actief						tinyint(1)					NOT NULL,
 PRIMARY KEY(id),
 FOREIGN KEY(categorie_id)    		REFERENCES productcategorie(id)
 );
@@ -55,38 +64,46 @@ FOREIGN KEY(klant_id)        	REFERENCES klant(id)
 CREATE TABLE orderregel (
   bestelling_id					INTEGER UNSIGNED         	NOT NULL,
   product_id                 	INTEGER UNSIGNED         	NOT NULL,
+  aantal						INTEGER UNSIGNED			NOT NULL,
 FOREIGN KEY(bestelling_id)  	REFERENCES bestelling(id),
 FOREIGN KEY(product_id)      	REFERENCES product(id)
 );
 
 
 INSERT into productcategorie (naam)
-VALUES 	("Dranken"),
-		("Pizza's"),
+VALUES 	("Pizza's"),
 		("Pasta's"),
-		("Lasagne's");
+		("Lasagne's"),
+		("Dranken");
 
-INSERT INTO `product` (`naam`, `omschrijving`, `categorie_id`, `prijs`) VALUES
-('Cola', NULL, 1,200),
-('Cola Light', NULL, 1, 200),
-('Sprite', NULL, 1, 200),
-('Sinas', NULL, 1,200),
-('Margherita', 'Tomaten en Kaas', 2,750),
-('Napoletana', 'Tomaten, kaas, kappertjes en ansjovis', 2, 950),
-('Funghi', 'Tomaten, kaas en champignons', 2, 950),
-('Prosciutto', 'Tomaten, kaas en ham', 2, 950),
-('Bolognese', 'Tomatensaus, gehakt en parmezaanse kaas', 3, 950),
-('Napoletana', 'Tomatensaus, knoflook en peterselie', 3,900),
-('Carbonara', 'Spek, ei, room, peterselie en uien', 3, 1050),
-('Frutti di Mare', 'Tomatensaus, vissoorten, tonijn en peterselie', 3,1050),
-('Lasagne della casa', 'Dunne pastabladen met room , bolognese saus ,mozzarella kaas en Parmezaanse kaas', 4, 1250),
-('Lasagne verde', 'Dunne pastabladen met room, bladspinazie, ricotta, kaas, pijnboompitten en Parmezaanse kaas af gegarneerd met walnoten', 4,1350),
-('Canneloni di mamma', 'Deegrolletjes gevuld met kalfsgehakt, ricotta kaas, pijnboompitten en rozijnen', 4,1475);
+INSERT INTO product (naam, omschrijving, categorie_id, prijs,actief) VALUES
+('Cola', NULL, 4,200,0),
+('Cola Light', NULL, 4, 200,0),
+('Sprite', NULL, 4, 200,0),
+('Sinas', NULL, 4,200,0),
+('Margherita', 'Tomaten en Kaas', 1,750,0),
+('Napoletana', 'Tomaten, kaas, kappertjes en ansjovis', 1, 950,0),
+('Funghi', 'Tomaten, kaas en champignons', 1, 950,0),
+('Prosciutto', 'Tomaten, kaas en ham', 1, 950,0),
+('Bolognese', 'Tomatensaus, gehakt en parmezaanse kaas', 2, 950,0),
+('Napoletana', 'Tomatensaus, knoflook en peterselie', 2,900,0),
+('Carbonara', 'Spek, ei, room, peterselie en uien', 2, 1050,0),
+('Frutti di Mare', 'Tomatensaus, vissoorten, tonijn en peterselie', 2,1050,0),
+('Lasagne della casa', 'Dunne pastabladen met room , bolognese saus ,mozzarella kaas en Parmezaanse kaas', 3, 1250,0),
+('Lasagne verde', 'Dunne pastabladen met room, bladspinazie, ricotta, kaas, pijnboompitten en Parmezaanse kaas af gegarneerd met walnoten', 3,1350,0),
+('Canneloni di mamma', 'Deegrolletjes gevuld met kalfsgehakt, ricotta kaas, pijnboompitten en rozijnen', 3,1475,0);
 
 
-INSERT into user (emailadres,password,naam)
-VALUES 	("p.a.wesseling@st.hanze.nl","$2y$10$t3F2hQKtQfPbkXnYoDD.1ujnt8ZjVf7EgkxTlJQdPVD7ZePJfZSTq","Alexander"),
-		("r.m.ten.klooster@st.hanze.nl","$2y$10$o9266kmjWhls.JFYUTaYuuL/.6frHMWf5RzGPRFcfSEruxn870TRS","Richard"),
-		("s.e.j.kuiper@st.hanze.nl","$2y$10$MYmpheu8fKa8.v7zu1m7ZuSe10YJSO7nXH7ZE2N1ZdMBqNtR5tblS","Samuel"),
-		("m.nieuwenweg@st.hanze.nl","$2y$10$xDlLTjHGubKVI21bQkXOtu90PU3tJIb8SvkVaNMPFZupwMdn2IHe.","Micheal"),
-		("t.a.van.der.linden@st.hanze.nl","$2y$10$/Cfc1jXVQJwZNBcZbjnv5eJqa/m/Vi8E6sVMyMWpSIXZajx/oQNsq","Thomas")		;
+INSERT into autorisatie (naam)
+VALUES 	("admin"),
+		("medewerker")
+		;
+
+INSERT into user (emailadres,password,naam,autorisatie_id)
+VALUES 	("p.a.wesseling@st.hanze.nl","$2y$10$t3F2hQKtQfPbkXnYoDD.1ujnt8ZjVf7EgkxTlJQdPVD7ZePJfZSTq","Alexander",1),
+		("r.m.ten.klooster@st.hanze.nl","$2y$10$o9266kmjWhls.JFYUTaYuuL/.6frHMWf5RzGPRFcfSEruxn870TRS","Richard",1),
+		("s.e.j.kuiper@st.hanze.nl","$2y$10$MYmpheu8fKa8.v7zu1m7ZuSe10YJSO7nXH7ZE2N1ZdMBqNtR5tblS","Samuel",1),
+		("m.nieuwenweg@st.hanze.nl","$2y$10$xDlLTjHGubKVI21bQkXOtu90PU3tJIb8SvkVaNMPFZupwMdn2IHe.","Micheal",1),
+		("t.a.van.der.linden@st.hanze.nl","$2y$10$/Cfc1jXVQJwZNBcZbjnv5eJqa/m/Vi8E6sVMyMWpSIXZajx/oQNsq","Thomas",1)		;
+
+
