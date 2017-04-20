aa3954dd757ac4fe72f83cc330ee1ccd6234a12d
abihas <info@michaelnieuwenweg.nl>
2017-04-20 10:49:08 +0200

    database

--- functions/database.php
+++ functions/database.php
@@ -1,17 +1,18 @@
 <?php
 $hostname = getHostName();
 
-if($hostname == "MacBook-Pro-van-Richard.local" || $hostname == "SURFACE-RICHARD" || $hostname = "LenovoY500"){
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "pizza";
-}else{
-$host = "localhost";
-$user = "abihas";
-$pass = "bdgoiusdfgjhs987r2342098fgdgbdk";
-$name = "pizza";
+if($hostname == "MacBook-Pro-van-Michael-2239.local") {
+    $host = "localhost";
+    $user = "abihas";
+    $pass = "bdgoiusdfgjhs987r2342098fgdgbdk";
+    $name = "pizza";
+} else {
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $name = "pizza";
 }
+
 try
 {
      $db = new PDO("mysql:host={$host};dbname={$name}",$user,$pass);
@@ -21,5 +22,4 @@ catch(PDOException $e)
 {
      //echo $e->getMessage(); # niet in productie gebruiken.
 }
-
 ?>
